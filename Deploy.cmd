fc6c2853bd3775e36dc461a9b399598f54a57572
Aaron Throckmorton <aaron@throckmortons.com>
2019-04-19 16:41:01 -0600

    Add deployment script and config for github pages

---
+++ Deploy.cmd
@@ -0,0 +1,17 @@
+
+@echo on
+
+setlocal
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/SharpSeeEr/resume.git master:gh-pages
+
+rmdir /s /q .git
+cd ..
