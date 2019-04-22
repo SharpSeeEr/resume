a2a33745688a57c4d865c0d8fa8cfe3f2e4c6a84
Aaron Throckmorton <aaron@throckmortons.com>
2019-04-22 14:08:18 -0600

    Add Education section

--- src/components/ResumeBody.vue
+++ src/components/ResumeBody.vue
@@ -1,46 +1,62 @@
 <template>
   <div class="container">
     <div class="header">
-      <h1 class="title">{{name}}</h1>
-      <h2 class="subtitle">{{title}}</h2>
+      <h1 class="name">{{name}}</h1>
+      <h2 class="profession">{{title}}</h2>
       <p class="summary">
         {{summary}}
       </p>
     </div>
 
     <div class="columns">
-      <div class="column column-main">
-        <div class="section">
-          <h2 class="heading">
-            <span class="fas fa-briefcase"></span> Experience
-          </h2>
-          <div v-for="(item, index) in experience" :key="index" class="experience">
-            <div class="dates">
-              <div>{{item.startYear}} - {{item.endYear || 'present'}}</div>
-            </div>
-            <div class="details">
-              <h3 class="position">{{item.position}}</h3>
-              <div class="company">{{item.company}}</div>
-              <experience-item v-for="(desc, index) in item.description" :key="index" :value="desc"></experience-item>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div class="column column-side">
         <div class="section">
-          <h2 class="heading">
+          <h2 class="title">
             <span class="fas fa-user"></span>Personal Info
           </h2>
           <personal-info v-for="item in personalInfo" :key="item.label" :value="item"></personal-info>
         </div>
 
         <div class="section">
-          <h2 class="heading">
+          <h2 class="title">
             <span class="fas fa-puzzle-piece"></span>Skills
           </h2>
           <div class="skills">
             <resume-skill v-for="item in skills" :key="item.skill" :skill="item.skill" :rating="item.rating"></resume-skill>
+          </div>
+        </div>
+      </div>
+
+      <div class="column column-main">
+        <div class="section">
+          <h2 class="title">
+            <span class="fas fa-briefcase"></span>Experience
+          </h2>
+          <div v-for="(item, index) in experience" :key="index" class="entry">
+            <p class="dates">
+              {{item.startYear}} - {{item.endYear || 'present'}}
+            </p>
+            <div class="details">
+              <h3 class="title">{{item.position}}</h3>
+              <div class="subtitle">{{item.company}}</div>
+              <experience-item v-for="(desc, index) in item.description" :key="index" :value="desc"></experience-item>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="education" class="section">
+          <h2 class="title">
+            <span class="fas fa-briefcase"></span>Education
+          </h2>
+          <div v-for="(item, index) in education" :key="index" class="entry">
+            <p class="dates">
+              {{item.startYear}} - {{item.endYear || 'present'}}
+            </p>
+            <div class="details">
+              <h3 class="title">{{item.schoolName}}</h3>
+              <div class="subtitle">{{item.degree}}</div>
+            </div>
           </div>
         </div>
       </div>
